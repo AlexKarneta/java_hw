755381f620a017865bff28a9beb9c0b71202ceba
Aliaksandr Karneta <aliaksandr.karneta@tvbet.tv>
2025-09-15 22:18:49 +0300

    feat:"lesson 4"

--- src/main/java/org/example/Lesson_3.java
+++ src/main/java/org/example/Lesson_3.java
@@ -1,43 +1,49 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Lesson_3 {
       //1
-        Random random = new Random();
-        Integer age = random.nextInt(100);
-        if (age < 18) {
-            System.out.println("Доступ запрещен");
-        } else if (age >= 18 && age <= 60) {
-            System.out.println("Доступ разрешен");
-        } else
-            System.out.println("Вы в зоне риска");
+      public static void main(String[] args) {
+          Random random = new Random();
+          Integer age = random.nextInt(100);
+          if (age < 18) {
+              System.out.println("Доступ запрещен");
+          } else if (age >= 18 && age <= 60) {
+              System.out.println("Доступ разрешен");
+          } else
+              System.out.println("Вы в зоне риска");
 
-        List<Integer> tenNumbers = new ArrayList<>() {};
-        int counter=1;
-        while( counter<=10){
-            tenNumbers.add(counter);
-            counter++;
-        }
-        //2
-        int numbers=1;
-        while (numbers<=10){
-            System.out.println(numbers);
-            numbers++;
-        }
-        //3
-        int limit=10;
-        while (limit>0){
-            System.out.println(limit);
-            limit--;
-        }
-        //4
-        int result=0;
-        int i =0;
-        while(i<tenNumbers.size()){
-            result+=tenNumbers.get(i);
-            i++;
-        }
-        System.out.println(result);
+          List<Integer> tenNumbers = new ArrayList<>() {};
+          int counter=1;
+          while( counter<=10){
+              tenNumbers.add(counter);
+              counter++;
+          }
+          //2
+          int numbers=1;
+          while (numbers<=10){
+              System.out.println(numbers);
+              numbers++;
+          }
+          //3
+          int limit=10;
+          while (limit>0){
+              System.out.println(limit);
+              limit--;
+          }
+          //4
+          int result=0;
+          int i =0;
+          while(i<tenNumbers.size()){
+              result+=tenNumbers.get(i);
+              i++;
+          }
+          System.out.println(result);
 
-    }
+      }
 }
+
+
---
+++ src/main/java/org/example/Lesson_4.java
@@ -0,0 +1,98 @@
+package org.example;
+
+import java.util.Random;
+
+public class Lesson_4 {
+    public static void main(String[] args) {
+        //1
+        Random random = new Random();
+        int randomNumber = random.nextInt(8);
+        String result = null;
+        switch (randomNumber) {
+            case 1:
+                result = "Monday";
+                break;
+            case 2:
+                result = "tusday";
+                break;
+            case 3:
+                result = "wednesday";
+                break;
+            case 4:
+                result = "thuesday";
+                break;
+            case 5:
+                result = "friday";
+                break;
+            case 6:
+                result = "saturday";
+                break;
+            case 7:
+                result = "sunday";
+                break;
+            default:
+                result = "Friday 13";
+                break;
+        }
+        System.out.println(result);
+        //2
+        enum Month {
+            January,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November,
+            December
+        }
+        Month month = Month.January;
+        System.out.println(month);
+        enum Seasons {
+            Spring,
+            Summer,
+            Autumn,
+            Winter
+        }
+        Seasons season = Seasons.Spring;
+
+        switch (month) {
+            case December:
+            case January:
+            case February:
+                season = Seasons.Winter;
+                break;
+            case August:
+            case July:
+            case June:
+                season = Seasons.Summer;
+                break;
+            case September:
+            case October:
+            case November:
+                season = Seasons.Autumn;
+                break;
+            case March:
+            case April:
+            case May:
+                season = Seasons.Spring;
+                break;
+        }
+        System.out.println(season);
+        //3
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(5 * i);
+        }
+        //4
+        String star = "*";
+        for (int i = 0; i < 5; i++) {
+            System.out.println();
+            for (int j = 0; j <= i; j++)
+                System.out.print(star);
+        }
+    }
+}
